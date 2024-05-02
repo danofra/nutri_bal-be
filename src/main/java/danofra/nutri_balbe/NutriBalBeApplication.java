228a2ef96e1692585ede92fa04130647f8ebb493
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-05-02 16:31:03 +0200

    Initial commit

---
+++ src/main/java/danofra/nutri_balbe/NutriBalBeApplication.java
@@ -0,0 +1,13 @@
+package danofra.nutri_balbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriBalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriBalBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/danofra/nutri_balbe/NutriBalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package danofra.nutri_balbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriBalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

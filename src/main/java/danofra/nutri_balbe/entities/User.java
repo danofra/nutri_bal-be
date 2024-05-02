7677937b0fe7c3ecf007e4c7baa82a9ed6469d73
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-05-02 20:30:18 +0200

    mod role to user, add implements to UserDetails

--- src/main/java/danofra/nutri_balbe/entities/User.java
+++ src/main/java/danofra/nutri_balbe/entities/User.java
@@ -1,5 +1,6 @@
 package danofra.nutri_balbe.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import danofra.nutri_balbe.enums.Gender;
 import danofra.nutri_balbe.enums.Physical_activity;
 import danofra.nutri_balbe.enums.Role;
@@ -7,15 +8,21 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Setter
 @Getter
 @NoArgsConstructor
+@JsonIgnoreProperties({"password", "role", "authorities", "accountNonExpired",
+        "credentialsNonExpired", "accountNonLocked", "enabled"})
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -32,8 +39,9 @@ public class User {
     private String city_of_residence;
     @Enumerated(EnumType.STRING)
     private Role role;
+    private String avatar;
 
-    public User(String name, String surname, String email, String password, LocalDate date_of_birth, Gender gender, Physical_activity physical_activity, String nationality, String city_of_residence, Role role) {
+    public User(String name, String surname, String email, String password, LocalDate date_of_birth, Gender gender, Physical_activity physical_activity, String nationality, String city_of_residence, String avatar) {
         this.name = name;
         this.surname = surname;
         this.email = email;
@@ -43,6 +51,37 @@ public class User {
         this.physical_activity = physical_activity;
         this.nationality = nationality;
         this.city_of_residence = city_of_residence;
-        this.role = role;
+        this.role = Role.USER;
+        this.avatar = avatar;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
